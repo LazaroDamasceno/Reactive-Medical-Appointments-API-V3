b36b94fa03e6d976fd1c3a48d656eb5a85fbab51
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-09 16:21:51 -0300

    Expanded the text cases of PersonRegistrationTest

--- src/test/kotlin/com/api/v3/PersonRegistrationTest.kt
+++ src/test/kotlin/com/api/v3/PersonRegistrationTest.kt
@@ -2,15 +2,13 @@ package com.api.v3
 
 import com.api.v3.people.domain.Person
 import com.api.v3.people.dtos.PersonRegistrationDto
+import com.api.v3.people.exceptions.DuplicatedEmailException
+import com.api.v3.people.exceptions.DuplicatedSsnException
 import com.api.v3.people.services.exposed.PersonRegistrationService
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.*
 import java.time.LocalDate
 
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
-import org.mockito.InjectMocks
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.assertEquals
@@ -20,10 +18,9 @@ import kotlin.test.assertEquals
 class PersonRegistrationTest {
 
 	@Autowired
-	@InjectMocks
 	private lateinit var personRegistrationService: PersonRegistrationService
 
-	private val registrationDto = PersonRegistrationDto(
+	private val registrationDto1 = PersonRegistrationDto(
 		"Leo",
 		"",
 		"Santos",
@@ -36,10 +33,37 @@ class PersonRegistrationTest {
 
 	@Test
 	@Order(1)
-	fun testSuccessful() = runBlocking {
-		val actual = personRegistrationService.register(registrationDto)::class
+	fun testSuccessful(): Unit = runBlocking {
+		val actual = personRegistrationService.register(registrationDto1)::class
 		val expected = Person::class
 		assertEquals(expected, actual)
 	}
 
+    @Test
+    @Order(2)
+    fun testUnSuccessfulForDuplicatedSsn(): Unit = runBlocking {
+        assertThrows<DuplicatedSsnException> {
+            personRegistrationService.register(registrationDto1)
+        }
+    }
+
+    private val registrationDto2 = PersonRegistrationDto(
+        "Leo",
+        "",
+        "Santos",
+        LocalDate.parse("2000-12-12"),
+        "123456788",
+        "male",
+        "leosantos@mail.com",
+        "1234567890"
+    )
+
+
+    @Test
+    @Order(3)
+    fun testUnSuccessfulForDuplicatedEmail(): Unit = runBlocking {
+        assertThrows<DuplicatedEmailException> {
+            personRegistrationService.register(registrationDto2)
+        }
+    }
 }
